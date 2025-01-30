b2158a77d416e81d62bd1a27a06b96693b2489e2
Dominion_rus <fatal1ty2013@yandex.ru>
2025-01-30 19:49:51 +0300

    fix style error

--- src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.dal.mappers.FilmRowMapper;
 import ru.yandex.practicum.filmorate.dal.mappers.UserRowMapper;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
